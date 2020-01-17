55f5801216f36417a1da552a53f501d38b9cb6a8
James Bradley <zenmonk3360@gmail.com>
2020-01-17 07:39:06 -0500

    some updates to Base64 encoder

--- Assessment/src/main/scala/Base64.scala
+++ Assessment/src/main/scala/Base64.scala
@@ -1,16 +1,38 @@
+import java.util.{Base64 => JavaBase64}
+
 object Base64 {
+  /**
+   * For the original implementation, I figured the test was to hand-code the
+   * conversion to base64, so I did it that way.
+   *
+   * Just for fun, I've also added a version that uses the Java library base64
+   * encoder, which is what I would do in "real life".
+   * ETA: except after testing, I realized that this version doesn't work
+   * correctly for odd-length input strings.  In that case, conversion to a
+   * byte array results in an extra 'A' at the end of the encoded result.  D'oh!
+   *
+   * I also cleaned up the pattern matches a bit.
+   *
+   * @param args
+   */
   def main(args: Array[String]): Unit = {
     if(args.length != 1) {
       println("usage")
       System.exit(1)
     }
     try {
-      val b64 = args(0).map(Character.digit(_, 16)).grouped(3).flatMap {
-        case grp if grp.length == 1 => Seq(grp(0) << 2)
-        case grp if grp.length == 2 => Seq(grp(0) << 2 | grp(1) >> 2, (grp(1) & 3) << 4)
-        case grp => Seq(grp(0) << 2 | grp(1) >> 2, (grp(1) & 3) << 4 | grp(2))
+      val hex = args(0).map(Character.digit(_, 16))
+      val b64 = hex.grouped(3).flatMap {
+        case Seq(a, b, c) => Seq(a << 2 | b >> 2, (b & 3) << 4 | c)
+        case Seq(a, b) => Seq(a << 2 | b >> 2, (b & 3) << 4)
+        case Seq(a) => Seq(a << 2)
       }.toSeq
-      println(s"${encode(b64)}")
+      println(encode(b64))
+      val bytes = hex.grouped(2).map {
+        case Seq(a, b) => (a << 4 | b).toByte
+        case Seq(a) => (a << 4).toByte
+      }.toArray
+      println(JavaBase64.getEncoder.encodeToString(bytes))
     } catch {
       case ex : Throwable =>
         println(s"error: ${ex.getMessage}")
